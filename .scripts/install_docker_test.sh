168418a8b80cb332a9125151c65c421538776feb
Eric Nemchik <eric@nemchik.com>
2019-07-22 09:51:59 -0500

    Continue working on test

--- .scripts/install_docker_test.sh
+++ .scripts/install_docker_test.sh
@@ -15,8 +15,6 @@ install_docker_test() {
         curl -fsSL test.docker.com -o "${GET_DOCKER}" > /dev/null 2>&1 || fatal "Failed to get docker test build install script."
         sh "${GET_DOCKER}" > /dev/null 2>&1 || fatal "Failed to install docker test build."
         rm -f "${GET_DOCKER}" || warning "Temporary test.docker.com file could not be removed."
-        local UPDATED_DOCKER
-        UPDATED_DOCKER=$( (docker --version 2> /dev/null || echo "0") | sed -E 's/.* version ([^,]*)(, build .*)?/\1/')
     fi
     PROMPT=${PREPROMPT:-}
 }
